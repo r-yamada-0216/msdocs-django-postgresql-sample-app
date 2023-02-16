348e284041244a487bc78e57150dacdefbdfd6f2
r-yamada-0216 <125527375+r-yamada-0216@users.noreply.github.com>
2023-02-16 15:10:06 +0900

    mod db'info

--- azureproject/production.py
+++ azureproject/production.py
@@ -32,9 +32,9 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': conn_str_params['postgresql-test-230216'],
+        'HOST': conn_str_params['postgresql-test-230216.postgres.database.azure.com'],
+        'USER': conn_str_params['postgresadmin'],
+        'PASSWORD': conn_str_params['P@ssw0rd123456789'],
     }
 }
